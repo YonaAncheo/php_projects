ce48514c818cb7c1127fb882ff6cd1295dae6683
Yonathan <j.ancheo.gonzalez@gmail.com>
2025-05-22 15:04:42 -0400

    This is the first documentation uploaded from the W10 repository. This is for understanding PHP for the Web Development course.

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+echo "Hello world</br>";
+
+// echo "Name: "
+$_name = "Yona";
+$_last_name = "ancheo";
+$_age = (8*4);
+
+echo "Name: $_name $_last_name and I'm $_age</br>";
+echo "This is the main page for my php-projects</br>";
